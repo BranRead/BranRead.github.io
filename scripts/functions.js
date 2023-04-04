7566fdb0f1fd4d16ed1a12f471b164b8c926d8f0
BranRead <branrtread@gmail.com>
2023-04-04 20:25:01 -0300

    Updating to new portfolio

---
+++ scripts/functions.js
@@ -0,0 +1,9 @@
+/*
+Can only be used on pages over https can implement later!
+function copyText(){
+        const email = document.getElementById("email").innerHTML;
+    
+    navigator.clipboard.writeText(email)
+    console.alert("Copied email: " + email);
+}
+*/
