f6f117251fbaf5231b25f2aba58eb7a430eb0919
BranRead <branrtread@gmail.com>
2023-12-22 01:14:40 -0400

    Inventory UI done for now
    Going to work on making the rest of the UI colour scheme match the new theme, and then start work on the house
    
    This will start with a search for new assets.

--- pokemon-game/scripts/inventoryMenu.js
+++ pokemon-game/scripts/inventoryMenu.js
@@ -1,10 +1,15 @@
 const inventoryMenu = {
 
+    isDescriptionOpen: false,
+
     display: (type) => {
         game.player.inventory.items.forEach((item, index) => {
             if(item.useCategory == type){
                 const inventoryItem = document.createElement('div');
                 inventoryItem.className = "inventoryItem"
+
+                const inventoryItemContainer = document.createElement("div");
+                inventoryItemContainer.className = "inventoryItemContainer";
     
                 const inventoryImg = document.createElement('div');
                 inventoryImg.className = "inventoryImg";
@@ -31,43 +36,52 @@ const inventoryMenu = {
 
                 const inventoryBtns = document.createElement('div');
                 inventoryBtns.className = "inventoryBtns";
+
+                const infoBtn = document.createElement('button');
+                infoBtn.textContent = "Info";
+                infoBtn.className = "infoBtn";
+                infoBtn.addEventListener("click", () => {
+                    game.player.inventoryWindow = true;
+                    if(!inventoryMenu.isDescriptionOpen){
+                        inventoryMenu.isDescriptionOpen = true;
+                        document.getElementById("inventoryFooter").style.display = "flex";
+                        document.querySelector("#itemTitleText").textContent = item.name;
+                        document.querySelector("#itemDescriptionText").textContent = item.description;
+                    } else {
+                        inventoryMenu.isDescriptionOpen = false;
+                        document.getElementById("inventoryFooter").style.display = "none";
+                    }
+                })
                 
-                const useBtn = document.createElement('div');
+                const useBtn = document.createElement('button');
                 useBtn.textContent = "Use";
                 useBtn.className = "useBtn";
                 useBtn.addEventListener('click', (e) => {
                     game.player.inventory.selection(e.currentTarget)
                 });
 
-                const trashBtn = document.createElement('div');
-                trashBtn.innerHTML = "Drop";
+                const trashBtn = document.createElement('button');
+                trashBtn.textContent = "Drop";
                 trashBtn.className = "trashBtn";
                 trashBtn.addEventListener('click', (e) => {
                     game.player.inventory.trash(e.currentTarget)
                 });
                 
+                inventoryBtns.append(infoBtn);
                 inventoryBtns.append(useBtn);
                 inventoryBtns.append(trashBtn);
     
                 inventoryItem.dataset.value = index;
-                inventoryNameText.innerHTML = item.name
-                inventoryQuantityText.innerHTML = `x${item.quantity}`;
+                inventoryNameText.textContent = item.name
+                inventoryQuantityText.textContent = `x${item.quantity}`;
     
-                inventoryItem.append(inventoryImg);
-                inventoryItem.append(inventoryName);
-                inventoryItem.append(inventoryQuantity);
+                inventoryItemContainer.append(inventoryImg);
+                inventoryItemContainer.append(inventoryName);
+                inventoryItemContainer.append(inventoryQuantity);
+                inventoryItem.append(inventoryItemContainer);
                 inventoryItem.append(inventoryBtns);
                 
-                inventoryItem.addEventListener("click", () => {
-                    game.player.inventoryWindow = true;
-                    document.querySelector("#itemTitle").style.display = "block";
-            document.querySelector("#itemDescription").style.display = "block";
-                    document.querySelector("#itemTitleText").innerHTML = item.name;
-                    document.querySelector("#itemDescriptionText").innerHTML = item.description;
-                })
-    
                 document.querySelector('#inventoryBody').append(inventoryItem)
-    
             }
         })
     }
