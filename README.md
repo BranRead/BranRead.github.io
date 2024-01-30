a0d3cc3fb503e3c17ef1100b06168a7a790a0eba
BranRead <branrtread@gmail.com>
2024-01-29 22:27:11 -0400

    In progress of fixing paths

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Portfolio
+> [!important]
+> Currently the paths don't work for the rpg game. It DOES seem to work if you click on it to play and then go back and click on it again. I will be fixing the paths very soon.
 ## General
 ### This was started as an assignment at NSCC but I will be maintaining it long after. It's very much a work in progress (Always will be by definition of a portfolio but is especially so, right now)
 ### Included in the files under pokemon-game is a game made with HTML, CSS and JS. It can be played directly in your browser, as long as your screen size is above what the canvas size is set to, it should work fine and be layed out how it's intended to be.
--- pokemon-game/data/audio.js
+++ pokemon-game/data/audio.js
@@ -1,42 +1,42 @@
 const audio = {
     Map: new Howl({
-        src: '/pokemon-game/audio/map.wav',
+        src: '../audio/map.wav',
         html5: true,
         volume: 0.1
     }),
 
     initBattle: new Howl({
-        src: '/pokemon-game/audio/initBattle.wav',
+        src: '../audio/initBattle.wav',
         html5: true,
         volume: 0.05
     }),
 
     battle: new Howl({
-        src: '/pokemon-game/audio/battle.mp3',
+        src: '../audio/battle.mp3',
         html5: true,
         volume: 0.1
     }),
 
     tackleHit: new Howl({
-        src: '/pokemon-game/audio/tackleHit.wav',
+        src: '../audio/tackleHit.wav',
         html5: true,
         volume: 0.1
     }),
 
     initFireball: new Howl({
-        src: '/pokemon-game/audio/initFireball.wav',
+        src: '../audio/initFireball.wav',
         html5: true,
         volume: 0.1
     }),
 
     fireballHit: new Howl({
-        src: '/pokemon-game/audio/fireballHit.wav',
+        src: '../audio/fireballHit.wav',
         html5: true,
         volume: 0.1
     }),
 
     victory: new Howl({
-        src: '/pokemon-game/audio/victory.wav',
+        src: '../audio/victory.wav',
         html5: true,
         volume: 0.5
     }),
