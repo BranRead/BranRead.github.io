b40fd936a88f9d03f3a31e7f1a84151b6afcde5e
BranRead <branrtread@gmail.com>
2024-02-12 14:55:11 -0400

    Added animated crow!

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,6 +1,7 @@
 const email = document.getElementById("email");
 const triangleCursor = document.getElementById("triangleCursor");
 const readMore = document.getElementById("advanceText");
+let animationID;
 
 const dialogOptions = new Map([
     ["intro", "Having loved computers and technology my entire life, I switched careers and went back to school for programming. At my core I truly care for others, place a high value on my work and always strive to do better. I bring my passion and dedication to everything I do."],
@@ -51,3 +52,68 @@ if(window.location.href == "https://branread.github.io/" || window.location.href
 
 email.addEventListener('click', copyEmail);
 readMore.addEventListener('click', dialog);
+
+
+if(window.location.href == "https://branread.github.io/" || window.location.href == "https://branread.github.io/index.html" || window.location.href == 'http://127.0.0.1:5501/index.html') {
+    const canvas = document.getElementById("crowScreen");
+    const context = canvas.getContext("2d");
+    canvas.width = 400;
+    canvas.height = 50;
+
+    if(screen.width < 577){
+        canvas.width = 280;
+    }
+    const crowImageRight = new Image();
+    crowImageRight.src = "/images/crowWalkRight.png";
+
+    const crowImageLeft = new Image();
+    crowImageLeft.src = "/images/crowWalkLeft.png";
+
+    const crow = new Sprite({
+        spritesheetPosition: {
+            x: 0,
+            y: 0
+        },
+        canvasPosition: {
+            x: 0,
+            y: 0 - 14
+        },
+        dimensions: {
+            width: 80,
+            height: 80
+        },
+        image: crowImageRight,
+        animate: true,
+        sprite: {
+            right: crowImageRight,
+            left: crowImageLeft
+        },
+        frames: {
+            max: 16,
+            hold: 10
+        }
+    })
+
+    crow.isGoingRight = true;
+
+    function animation(){
+        animationID = window.requestAnimationFrame(animation);
+        if(crow.isGoingRight) {
+            crow.canvasPosition.x += 1;
+        } else {
+            crow.canvasPosition.x -= 1;
+        }
+
+        if(crow.canvasPosition.x > canvas.width){
+            crow.isGoingRight = false;
+            crow.image = crow.sprite.left;
+        } else if (crow.canvasPosition.x < 0 - crow.dimensions.width) {
+            crow.isGoingRight = true;
+            crow.image = crow.sprite.right;
+        }
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        crow.draw(context);
+    }
+
+    animation();
+}
---
+++ scripts/sprite.js
@@ -0,0 +1,96 @@
+class Sprite {
+    constructor({
+                    // Position on spritesheet
+                    spritesheetPosition = {
+                        x: 0,
+                        y: 0
+                    },
+
+                    // Position on gamemap
+                    canvasPosition = {
+                        x: 0,
+                        y: 0
+                    },
+
+                    // key value pair of width and height (width, height)
+                    dimensions,
+
+                    image,
+
+
+                    // boolean to control animation
+                    animate,
+                    sprite,
+                    frames = { max: 1, hold: 10 },
+                }) {
+
+        this.spritesheetPosition = spritesheetPosition
+        this.canvasPosition = canvasPosition
+        this.dimensions = dimensions
+        this.image = image
+        this.image.onload = () => {
+
+        }
+        this.frames = {...frames, val: 0, elapsed: 0}
+        this.image.src = image.src
+        this.sprite = sprite
+        this.animate = animate
+        this.opacity = 1
+    }
+
+    draw(context) {
+        // console.log(this.image.src)
+        context.save()
+        context.translate(
+            this.canvasPosition.x + this.dimensions.width / 2,
+            this.canvasPosition.y + this.dimensions.height / 2
+        )
+
+        context.translate(
+            -this.canvasPosition.x - this.dimensions.width / 2,
+            -this.canvasPosition.y - this.dimensions.height / 2
+        )
+        // Takes image, starting x position and starting y position to start drawing from, starting width and height for size from spritesheet
+        // then takes destination position and dimensions
+        // (image, sx, sy, swidth, sheigth, dx, dy, dwidth, dheight)
+        context.drawImage(
+            this.image,
+
+            // sx
+            this.spritesheetPosition.x + this.dimensions.width * this.frames.val,
+
+            // sy
+            this.spritesheetPosition.y,
+
+            // swidth
+            this.dimensions.width,
+
+            // sheight
+            this.dimensions.height,
+
+            // dx
+            this.canvasPosition.x,
+
+            // dy
+            this.canvasPosition.y,
+
+            // dwidth
+            this.dimensions.width,
+
+            // dheight
+            this.dimensions.height
+        );
+        context.restore()
+
+        if(!this.animate) return
+
+        if(this.frames.max > 1) {
+            this.frames.elapsed++
+        }
+
+        if(this.frames.elapsed % this.frames.hold === 0) {
+            if(this.frames.val < this.frames.max - 1) this.frames.val++
+            else this.frames.val = 0
+        }
+    }
+}
